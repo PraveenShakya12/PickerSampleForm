e7dd7a449fdd80222d8dcb82fc4d06fa6ac52999
Praveen Shakya IndiaNIC <praveen.shakya@indianic.com>
2022-12-16 19:12:20 +0530

    feat/added redux-persist

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigator from './src/navigations/MainNavigator';
 import {Provider} from 'react-redux';
-import store from './src/redux/store/store';
+import store, {persistor} from './src/redux/store/store';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
